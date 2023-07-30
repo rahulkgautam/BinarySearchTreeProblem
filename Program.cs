561c8e41933aef6e5b7ed8cfe70fdecbafb8743c
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-30 23:09:44 +0530

    Welcome to Binary Search Tree program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+        }
+    }
+}
